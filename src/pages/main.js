55babb9ed4b33606ad9cf9d47dcbb977dc0529be
klu_2200031618 <kk258786@gmail.com>
2024-06-12 15:34:04 +0530

    Project UI has been completed, Need to add game logic

--- src/pages/main.js
+++ src/pages/main.js
@@ -8,7 +8,21 @@ export const Main=()=>{
             <Welcome />
 
             <div className='game-section'>
-
+                <div className='game-turns'>
+                    <span>1. Ensure to have two players before start</span>
+                    <span>2. Make sure to get each player one turn at one time</span>
+                </div>
+                <div className='game-space'>
+                    <img src={require('../images/b.png')} className='each-img' alt='b-img'/>
+                    <img src={require('../images/b.png')} className='each-img' alt='b-img'/>
+                    <img src={require('../images/b.png')} className='each-img' alt='b-img'/>
+                    <img src={require('../images/b.png')} className='each-img' alt='b-img'/>
+                    <img src={require('../images/b.png')} className='each-img' alt='b-img'/>
+                    <img src={require('../images/b.png')} className='each-img' alt='b-img'/>
+                    <img src={require('../images/b.png')} className='each-img' alt='b-img'/>
+                    <img src={require('../images/b.png')} className='each-img' alt='b-img'/>
+                    <img src={require('../images/b.png')} className='each-img' alt='b-img'/>
+                </div>
             </div>
         
         </div>
